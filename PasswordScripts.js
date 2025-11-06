b702842228bb9cfc6306b19d4597ac5091645128
BL Allen <beth.lacy.allen@gmail.com>
2025-11-06 01:43:05 +0000

    Create a secure(ish) admin area

---
+++ PasswordScripts.js
@@ -0,0 +1,10 @@
+let password = "STEVO";
+(function passcodeprotect() {
+    let passcode = prompt("Enter Password");
+    while (passcode !== password) {
+        alert("Incorrect Password");
+        return passcodeprotect();
+    }
+    }());
+    alert('Welcome :)');
+    document.getElementById("container").style.display = "block";
